6955d759a9af8a9332e9ab02fedf0d80a5b489f7
Daniel !Gao-!gaseb <91456983+Agabe-gaogaseb07@users.noreply.github.com>
2022-02-13 07:17:34 +0000

    Add files via upload

---
+++ BMI calculator.py
@@ -0,0 +1,12 @@
+
+#Prompts the needed details with the float and the input function.
+#The float rounds all decimals to a whole number.
+
+height = float(input("Enter your height in m: "))
+weight = float(input("Enter your weight in kg?: "))
+
+#Calculates the Body Mass Index using the weight and height entered to the power of 2.
+BMI = float(int(weight) / int(height**2))
+
+#Using the f-string it will print out what is your Body Mass Index(BMI) using the If statement.
+
